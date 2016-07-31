184582c929dc655eb3537ee6139ef6d5becd4a5f
Jesus Castaneda <jesco.marketing@gmail.com>
2016-07-30 23:29:10 -0500

    Add styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
-    
+
 $(document).ready(function(){
-    $(".msg").hide();
+	var width = screen.width -80;
+	width = width;
+	$(".cover").css("height", screen.height);
+	$(".menu").css("margin-left", width + "px");
+
     $(".menu li").mouseenter(function(){
         $(this).css({"margin-left": "-90px", "transition": "0.5s"});
     });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
         </div>
         <div class="row">
             <div class="cover">
-                <div class="col-md-3 col-md-offset-9 hidden-lg menu-xs text-center">
+                <div class="col-md-4 col-md-offset-4 hidden-lg menu-xs text-center">
                     <ul class="list-unstyled list-inline">
                         <li id="home" >
                             <a href="#"><i class="glyphicon glyphicon-home"></i> Home</a>
@@ -75,15 +75,16 @@
                 <div class="col-md-4 col-md-offset-4 text-center social">
                     <h1>Jesús Castañeda<br><small>< Full Stack Web Developer /></small></h1>
                     <br>
+                    <h3 class="hidden-xs text-success">Follow me on Social Media</h3>
                     <a href="http://www.github.com/jesuscastaneda86" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Github" src="images/github.png" alt="G"></a>
                     <a href="https://mx.linkedin.com/in/jesús-manuel-castañeda-olmos-06a27ab7" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Linkedin" src="images/linkedin.png" alt="linkedin"></a>
-                    <a href="http://www.codepen.io/jesuscastaneda86" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Codepen" src="images/codepen.png" alt="codepen"></a>
+                    <a href="http://www.codepen.io/jesuscastaneda86" class="hidden-xs" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Codepen" src="images/codepen.png" alt="codepen"></a>
                     <a href="https://www.freecodecamp.com/jesuscastaneda86" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Free Code Camp" src="images/fcc.png" alt="freecodecamp"></a>
                     <a href="http://www.sololearn.com/Profile/1171988" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Sololearn" src="images/sololearn.png" alt="sololearn"></a>
                     <a href="https://www.facebook.com/jesuscastanedamty" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Facebook" src="images/facebook.png" alt="facebook"></a>
                     <a href="https://twitter.com/jesus_castaneda" target="_blank"><img data-toggle="tooltip" data-placement="bottom" title="Twitter" src="images/twitter.png" alt="twitter"></a>
                 </div>
-                <div class="col-md-12 text-center copy hidden-xs">
+                <div class="col-md-12 copy hidden-xs">
                     <p>@Copyright 2016 Derechos Reservados | Created by: Jesús Castañeda</p>
                 </div>
             </div>
